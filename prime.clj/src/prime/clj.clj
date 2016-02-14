a41dbbba1bfe9e34a31eb37ae6aaaac8e44b10eb
Adar Nimrod <nimrod@shore.co.il>
2016-02-14 16:16:49 +0200

    The Clojure version is now more functional, no mutable state (and without any stack overflows).

--- prime.clj/src/prime/clj.clj
+++ prime.clj/src/prime/clj.clj
@@ -10,12 +10,17 @@
        (fn [y] (not= (mod x y) 0))
        (range 2 (math/ceil (math/sqrt x))))))
 
+(defn primes-iter [n, i, v]
+  (loop [cnt i acc v]
+    (if (= (count acc) n) acc
+      (recur (inc cnt) (if (prime? cnt) (conj acc cnt) acc)))))
+
+(defn primes [n]
+  (if
+    (zero? n) []
+    (primes-iter n 0 [])))
+
 
 (defn -main
   [& args]
-  (def v [])
-  (def i 0)
-  (while (< (count v) (Integer/parseInt (first args)))
-    (cond (prime? i) (def v (conj v i)))
-    (def i (inc i)))
-  (println v))
+  (println (primes (Integer/parseInt (first args)))))
